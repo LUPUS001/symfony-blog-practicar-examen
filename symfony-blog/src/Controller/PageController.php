f23362da29d0ab44d489d2988f2c1d65d6a371af
LUPUS001 <antoniodaw23@gmail.com>
2026-02-16 20:04:53 +0000

    2.2.2 y 2.2.3 - Validación, Persistencia en BD y Reto Thank You

--- symfony-blog/src/Controller/PageController.php
+++ symfony-blog/src/Controller/PageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,13 +25,28 @@ final class PageController extends AbstractController
     }
 
     #[Route('/contact', name: 'contact')]
-    public function contact(Request $request): Response
+    public function contact(ManagerRegistry $doctrine, Request $request): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactFormType::class, $contact);
         
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contacto = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($contacto);
+            $entityManager->flush();
+            return $this->redirectToRoute('thankyou');
+        }
+
         return $this->render('page/contact.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    #[Route('/thankyou', name: 'thankyou')]
+    public function thankyou(): Response
+    {
+        return $this->render('page/thankyou.html.twig');
     }
 }
